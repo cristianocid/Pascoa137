bb805496177e660e99b453e510299f4010a04ddf
CrisDang3r <105089039+CrisDang3r@users.noreply.github.com>
2023-04-01 13:56:13 -0300

    Teste Unitario Calculadora Simples

--- src/test/java/unitTest/testeCalculadora.java
+++ src/test/java/unitTest/testeCalculadora.java
@@ -10,7 +10,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 // Classe
-public class TesteCalculadora {
+public class testeCalculadora {
     // Atributos
 
     // Funções e Métodos
--- src/test/java/unitTest/testeCalculadoraSimples.java
+++ src/test/java/unitTest/testeCalculadoraSimples.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class TesteCalculadoraSimples {
+public class testeCalculadoraSimples {
 
     @Test
     public void testeAreaQuadrado(){
