859e0c00e0ff56b3d4cee3ae7f9e76ecec3062e9
cristianocid <crisdangerous@gmail.com>
2023-05-17 18:32:22 -0300

    Teste Atutomatizado Web Blazedemo com PageObject

--- src/test/java/unitTest/testeCalculadoraCSV.java
+++ src/test/java/unitTest/testeCalculadoraCSV.java
@@ -10,7 +10,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 // Classe
-public class testeCalculadora {
+public class testeCalculadoraCSV {
     // Atributos
 
     // Funções e Métodos
